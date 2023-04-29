1b0c4b4bd85131f5f4d4589f4b9aabdf834ffc95
Zeinab Moayeri <89657398+ZeinabMoayeri@users.noreply.github.com>
2023-04-29 12:51:02 +0330

    Update sobel_operator_test.py

--- sobel_operator_test.py
+++ sobel_operator_test.py
@@ -26,23 +26,6 @@ F1 = np.array([[-1, 0, 0],
 
 Output1 = fillter(Image, F1)
 
-# plt.imshow(Image, cmap='gray')
-# plt.title('Original Image')
-# plt.show()
-
-# Fx = np.array([[-1, 0, +1],
-#                [-2, 0, +2],
-#                [-1, 0, +1]])
-#
-# h0, w0 = Image.shape
-# s1, s2 = Fx.shape
-# h, w = h0-s1+1, w0-s2+1
-#
-# Gx = np.zeros((h, w))
-# for i in range(h):
-#     for j in range(w):
-#         Gx[i, j] = np.multiply(Image[i:i+s1, j:j+s2], Fx).sum()
-#
 plt.imshow(Output1, cmap='gray')
 plt.title('Output1')
 plt.show()
